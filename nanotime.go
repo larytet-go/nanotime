3302953d3d5d316e2ad83f9fb4b5e8f542579a66
arkady <larytet@yahoo.com>
2019-11-12 15:11:12 +0200

    Update

--- nanotime.go
+++ nanotime.go
@@ -1,9 +1,14 @@
 package nanotime
 
+import (
+	_ "unsafe"
+)
+
 //go:noescape
 //go:linkname nanotime runtime.nanotime
 func nanotime() int64
 
+// Now returns a timestamp
 func Now() int64 {
 	return nanotime()
 }
